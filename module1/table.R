9960e46be79b426f38ea152f53d2a22f64ef1219
facosta8 <facosta8@gmail.com>
2019-10-16 22:05:20 -0500

    first module exercises

---
+++ module1/table.R
@@ -0,0 +1,23 @@
+library(rvest)
+library(tibble)
+library(rio)
+
+url <- "https://ec.europa.eu/eurostat/tgm/table.do?tab=table&init=1&plugin=1&language=en&pcode=tec00115"
+
+gdp_table <- read_html(url) %>% 
+    html_nodes('table') %>% 
+    html_table()
+
+column_names <- names(gdp_table[[4]])
+country_list <- c(names(gdp_table[[5]]), gdp_table[[5]][[1]])
+country_values <- gdp_table[[6]]
+                      
+gdp <- as_tibble(country_values)
+colnames(gdp) <- column_names
+gdp$country <- country_list
+
+gdp %>% 
+    dplyr::select(country, `2007`:`2018`)
+
+
+export(gdp, 'gdp.csv')
